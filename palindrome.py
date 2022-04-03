ac5282cd23ce0cf2ebd88223ba8b7b746f25323b
Krystian Gumulinski <k.gumulinski@gmail.com>
2022-04-03 14:14:54 +0200

    Changed comment

--- palindrome.py
+++ palindrome.py
@@ -8,7 +8,6 @@ if question marks were swapped for certain letters.
 For strings with no question marks, it just checks if a string is a palindrome.
 """
 
-
 def check_if_palindrome(strng: str) -> bool:
     """This function return True or False, depending if given string is a palindrome or not"""
     if len(strng) % 2:
@@ -42,7 +41,7 @@ def replace_question_marks(strng: str) -> list[str]:
 
 def main():
     os.system("cls")
-    #For more than 4 ? characters, then prepare for long computation time.
+    #For more than 4 '?' characters, prepare for long computation time.
     examples = ["???", "a?a", "kayak", "dojo", "bambi", "?trer??", "?s?dA?dAsd?"]
     for example in examples:
         print(f"{example}:\t{len(replace_question_marks(example))}")
