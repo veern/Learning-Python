38fe9120d5445584ef2836d56c6bbc2cd70fba2a
Krystian <k.gumulinski@gmail.com>
2022-04-08 18:47:25 +0200

    Finalized changes to PhoneBook

--- PhoneBook/main.py
+++ PhoneBook/main.py
@@ -1,12 +1,13 @@
 from PhoneBook import PhoneBook, Contact
 import requests
 import html
+import os
 
 ENDPOINT = 'https://randomuser.me/api/'
 AMOUNT_TO_GENERATE = 3
 
 def call_request(url, params=[]):
-    r = requests.get(url)
+    r = requests.get(url, params=params)
     r.raise_for_status()
     return r
 # parameters = {"amount": 20}
@@ -24,17 +25,43 @@ def generate_sample_PhoneBook(amount: int) -> PhoneBook:
         Book.add_contact(person)
     return Book
 
+def engine() -> None:
+    os.system("cls")
+    possible_actions = ["SAVE", "LOAD", "ADD", "DELETE", "DISPLAY", "SAMPLE"]
+    print("Welcome to the phone book simulator.\nFollowing actions are possible:\n1.Load a PhoneBook\n2.Save a PhoneBook\n3.Add a contact\n4.Delete a contact\n5.Display all contacts\n")
+    phone_book = PhoneBook()
+    while True:
+        action = input(f'Your choice of action {possible_actions}: ').upper()
+        if action not in possible_actions:
+            continue
+        elif action == "SAVE":
+            address = input("Give a full path to where to save this PhoneBook to: ")
+            name_of_phone_book_file = input("Name the file to save the PhoneBook: ")
+            phone_book.save(f"{address}\{name_of_phone_book_file}")
+        elif action == "LOAD":
+            while True:
+                try:
+                    address_of_file_to_load = input("Give a full path of a file to load a PhoneBook: ")
+                    break
+                except FileNotFoundError:
+                    print("File not found!")
+            phone_book.load(address_of_file_to_load)
+        elif action == "ADD":
+            person = input("Give a persons name, lastname and phone number, each separated by a single space: ")
+            person = person.split(" ")
+            contact = Contact(person[0], person[1], int(person[2]))
+            phone_book.add_contact(contact)
+        elif action == "DELETE":
+            person = input("Give a persons name and lastname, each separated by a single space, that you want to delete from a phone book: ") 
+            person = person.split(" ")
+            phone_book.del_contact(person[0], person[1])
+        elif action == "DISPLAY":
+            print(phone_book)
+        elif action == "SAMPLE":
+            phone_book = generate_sample_PhoneBook(AMOUNT_TO_GENERATE)
+
 def main():
-    # Book = generate_sample_PhoneBook(AMOUNT_TO_GENERATE)
-    # Book.save(r"C:\Users\Krystian-Laptop\Desktop\Python\Moje\Katas\PhoneBook\Phonebook.txt")
-    Book = PhoneBook()
-    Book.load(r"C:\Users\Krystian-Laptop\Desktop\Python\Moje\Katas\PhoneBook\Phonebook.txt")
-    Shane = Contact("Shane", "Dawson", 239238123)
-    Dave = Contact("Dave", "Tribiani", 123456789)
-    Book.add_contact(Dave, Shane)
-    Book.del_contact("Dave", "Tribiani")
-    print(Book.contacts)
-    # print(Book)
+    engine()
 
 if __name__ == "__main__":
     main()
