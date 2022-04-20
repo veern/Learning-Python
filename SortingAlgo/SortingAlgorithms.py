121c3e7e836d8a195937f18c8423e89efa50d936
Krystian Gumulinski <k.gumulinski@gmail.com>
2022-04-20 16:12:55 +0200

    Fleshed out formating of the code

--- SortingAlgo/SortingAlgorithms.py
+++ SortingAlgo/SortingAlgorithms.py
@@ -2,11 +2,13 @@ def check_for_zero_or_one_len(lst: list) -> bool:
     if lst == [] or len(lst) == 1:
         return True
 
+
 def check_all_elements(lst: list) -> None:
     if all(isinstance(x, complex) for x in lst):
         raise TypeError("Complex sorting is not implemented")
     elif not all(isinstance(x, (int, float)) for x in lst) or not lst:
         raise TypeError("Not all values in the passed list are integers or floats")
+
 
 def bubble_sort(lst: list[int]) -> list[int]:
     """
@@ -16,24 +18,27 @@ def bubble_sort(lst: list[int]) -> list[int]:
     if check_for_zero_or_one_len(lst): return lst
     check_all_elements(lst)
 
-    N = len(lst)
+    n = len(lst)
     no_swap = True
-    for _ in range(N):
-        for j in range(N-1):
+    for _ in range(n):
+        for j in range(n-1):
             if lst[j] > lst[j+1]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
                 no_swap = False
-        if no_swap: # If no swap was done during the iteration of a list, then it is already sorted and further iteration is not needed
+        # If no swap was done during the iteration of a list, then it is already sorted and further iteration is not needed
+        if no_swap:
             return lst
-        N -= 1
+        n -= 1
     return lst
+
 
 def insert_sort(lst: list[int]) -> list[int]:
     """
     Insert sort goes over the list of numbers once and each time a number on the right is bigger than
     the one on the left, function iterates backwards to find its correct spot, shifting all elements to the right
     """
-    if check_for_zero_or_one_len(lst): return lst
+    if check_for_zero_or_one_len(lst):
+        return lst
     check_all_elements(lst)
     
     for i in range(len(lst)):
@@ -42,6 +47,7 @@ def insert_sort(lst: list[int]) -> list[int]:
             lst[j], lst[j-1] = lst[j-1], lst[j]
             j -= 1
     return lst
+
 
 def selection_sort(lst: list[int]) -> list[int]:
     """
@@ -52,13 +58,13 @@ def selection_sort(lst: list[int]) -> list[int]:
     if check_for_zero_or_one_len(lst): return lst
     check_all_elements(lst)
     
-    N = 0
+    n = 0
     for i in range(len(lst)):
         current_minimum = i
-        for j in range(N, len(lst)):
+        for j in range(n, len(lst)):
             if lst[current_minimum] > lst[j]:
                 current_minimum = j
-        if current_minimum != i: # if no new current_minimum was found, then the swap is not needed
+        if current_minimum != i:  # if no new current_minimum was found, then the swap is not needed
             lst[i], lst[current_minimum] = lst[current_minimum], lst[i]
-        N += 1
+        n += 1
     return lst
